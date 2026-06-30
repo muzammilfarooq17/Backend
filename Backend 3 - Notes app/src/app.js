91addec7820836c9c60749cbabd11667b066d595
muzammilfarooq17 <farooqmuzamil.127@gmail.com>
2026-07-01 00:36:57 +0500

    -

--- Backend 3 - Notes app/src/app.js
+++ Backend 3 - Notes app/src/app.js
@@ -1,7 +1,31 @@
 // main kaam hai is file ka server ko create krna . 
+// POST is used to send data to the server and create a new resource.
+
+// Postman (Short Definition):
+
+// Postman is an API testing tool used by developers to send API requests and view API responses without writing frontend code.
+
+// Uses of Postman:
+
+// Test APIs
+// Send GET, POST, PUT, DELETE requests
+// View server responses
+// Debug APIs
+
+// One-line Exam Definition:
+
+// Postman is an API testing tool used to send requests to APIs and check their responses.
 const express = require('express')
 const app = express()
 
+
+const notes = [];
+
+app.post('/notes',(req,res)=>{
+    console.log(req.body);
+    
+    
+})
 
 
 module.exports = app
