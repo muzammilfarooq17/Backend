ad4c2b9a210fc69df4dc6765b5293b1450a9c992
muzammilfarooq17 <farooqmuzamil.127@gmail.com>
2026-07-01 07:46:08 +0500

    -

--- Backend 3 - Notes app/src/app.js
+++ Backend 3 - Notes app/src/app.js
@@ -17,15 +17,52 @@
 // Postman is an API testing tool used to send requests to APIs and check their responses.
 const express = require('express')
 const app = express()
-
+app.use(express.json()) // it is a middlewere which allows to get readable data to express body in raw format ..
 
 const notes = [];
-
+//post req notes
 app.post('/notes',(req,res)=>{
-    console.log(req.body);
-    
-    
+    notes.push(req.body)
+    res.status(201).json({                     // server pr data bhejne kai lye
+        message:"notes created Sucessfully."
+    }) 
 })
 
+//get request notes
+app.get("/notes",(req,res)=>{
+    res.status(200).json({
+        message:"notes fetch Sucessfully.", // server sai data mangwane kai lye
+        notes:notes
+    })
+})
+
+// delete request Notes/index
+app.delete("/notes/:index",(req,res)=>{
+    const index = req.params.index
+    delete notes [index]              // server data pr ho or delete krne kai lye
+    res.status(200).json({
+        message:"Note Deleted Sucessfully"
+
+    })
+})
+
+app.patch("/notes/:index", (req, res) => {
+    // Convert the string parameter (like "1") into a real JavaScript number (1)
+    const index = Number(req.params.index); 
+    const title = req.body.title;
+
+    // Now it will correctly check notes[1]
+    if (notes[index] === undefined || notes[index] === null) {
+        return res.status(404).json({
+            message: "Note not found at this index!"
+        });
+    }
+
+    notes[index].title = title;
+    
+    res.status(200).json({
+        message: "Note Updated Successfully"
+    });
+});
 
 module.exports = app
