440fdebf3e2695defdaf051a88d4871a5b6bc440
muzammilfarooq17 <farooqmuzamil.127@gmail.com>
2026-07-25 08:16:25 +0500

    -

--- Backend 6 Authentication Implementation/src/app.js
+++ Backend 6 Authentication Implementation/src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const authRoutes = require('./src/routes/auth.routes');
+const authRoutes = require('./routes/auth.routes');
+const cookieParser = require('cookie-parser')
 
 const app = express();
-
 app.use(express.json());
+app.use(cookieParser())
 
-app.use('/auth', authRoutes);
+app.use('/api/auth', authRoutes);
 
 module.exports = app;
--- Backend 6 Authentication Implementation/src/routes/auth.routes.js
+++ Backend 6 Authentication Implementation/src/routes/auth.routes.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const authController = require('../controllers/auth.controller');
+const { router } = require('../app');
 
 const routes = express.Router();
 
 routes.post('/Register', authController.registerUser);
+routes.get('/test',(req,res)=>{
+    console.log("cookies:",req.cookies)
+    res.json({
+        message:"Test route",
+        cookies: req.cookies
+    })
+    
+})
 
 module.exports = routes;
