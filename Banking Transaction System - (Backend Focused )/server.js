700e958b3edfb90f3ed3b265bdd863ce3524cddf
muzammilfarooq17 <farooqmuzamil.127@gmail.com>
2026-08-19 08:30:15 +0500

    -

--- Banking Transaction System - (Backend Focused )/server.js
+++ Banking Transaction System - (Backend Focused )/server.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const app = require("./src/app")
+const connectDB = require('./src/config/db')
 
-// const PORT = 3000;
-
+connectDB()
 
 app.listen(3000,()=>{
     console.log("Server Is Running on http://localhost:3000");
